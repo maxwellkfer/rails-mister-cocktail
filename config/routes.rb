69c0537768df4311f287ba224c14ee8ec325cf93
Maxime GUERIN <maxwellkfer@gmail.com>
2018-08-03 12:50:19 +0100

    root changed and cards added

--- config/routes.rb
+++ config/routes.rb
@@ -4,8 +4,9 @@ Rails.application.routes.draw do
   get 'cocktails/show'
   get 'cocktails/new'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  resources :cocktails, only: [:index, :show, :new, :create ] do
+  resources :cocktails, only: [:show, :new, :create ] do
     resources :doses, only: [:new, :create ]
   end
   resources :doses, only: [:destroy]
+  root to: "cocktails#index"
 end
